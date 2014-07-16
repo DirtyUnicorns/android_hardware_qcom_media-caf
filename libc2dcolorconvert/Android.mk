49ee489d6fdb537e0445f6848f840b4167bf4894
Dorian Snyder <dastin1015@gmail.com>
2014-07-16 00:19:30 -0700

    Merge remote-tracking branch 'cm/cm-11.0' into du44
    Change-Id: Idd564a7d8779290419eca44e959dbb535d3c0e31

--- libc2dcolorconvert/Android.mk
+++ libc2dcolorconvert/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf)
 DISPLAY := display-caf
 else
-DISPLAY := display
+DISPLAY := display/$(TARGET_BOARD_PLATFORM)
 endif
 
 LOCAL_SRC_FILES := \
@@ -15,7 +15,9 @@ LOCAL_C_INCLUDES := \
     $(TOP)/frameworks/av/include/media/stagefright \
     $(TOP)/frameworks/native/include/media/openmax \
     $(TOP)/hardware/qcom/$(DISPLAY)/libcopybit
+
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+
 LOCAL_SHARED_LIBRARIES := liblog libdl
 
 LOCAL_MODULE_TAGS := optional
